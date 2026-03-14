b18a147066828ba6a0625d7fe30d1efd6ee05226
anksendeptri <anksendeptri@gmail.com>
2026-03-14 09:35:51 +0700

    sua bai tap 4

--- Bai-tap-TH1/Bai-tap-4.cpp
+++ Bai-tap-TH1/Bai-tap-4.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 using namespace std;
 // ham kiem tra nam nhuan
-bool namNhuan(y)
+bool namNhuan(int y)
 {
-    if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) return 1;
-    else return 0;
+    return (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0));
+    return 0;
 }
 // kiem tra so ngay trong thang m
 int soNgay(int m, int y)
@@ -21,13 +21,38 @@ int soNgay(int m, int y)
 void Nhap(int &d, int &m, int &y)
 {
     //neu nhap ky tu la hoac ngay khong hop le thi nhap lai
-    while(!(cin >> d >> m >> y) || d < 1 || d > soNgay(m, y) || m < 1 || m > 12 || y < 1)
+    while(!(cin >> d >> m >> y) || m < 1 || m > 12 || y < 1 || d < 1 || d > soNgay(m, y))
     {
         cout << "Nhap sai! Yeu cau nhap lai:\n";
+        //xoa loi nhap va ky tu loi
+        cin.clear();
+        cin.ignore(100, '\n');
     }
+}
+
+// ham xuat ngay ke tiep
+void nextDay(int d, int m, int y)
+{
+    d++;
+    // neu vuot qua so ngay thi qua thang moi
+    if (d > soNgay(m, y))
+    {
+        d = 1;
+        m ++;
+    }
+    //thang vuot qua 12 thi qua nam moi
+    if(m > 12)
+    {
+        m = 1;
+        y++;
+    }
+    cout << d << "/" << m << "/" << y << endl;
 }
 int main()
 {
     int d, m, y;
+    cout << "Nhap ngay thang nam: \n";
+    Nhap(d, m, y);
+    nextDay(d, m, y);
     return 0;
 }
