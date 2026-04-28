1deed462a14305c0489cd498a98279bfc71a90c8
anksendeptri <anksendeptri@gmail.com>
2026-04-28 23:04:38 +0700

    them comment

--- Bai-tap-TH3/Bai-4/Bai-4.cpp
+++ Bai-tap-TH3/Bai-4/Bai-4.cpp
@@ -10,19 +10,25 @@ int main()
 	cout << "Mang ngau nhien: \n";
 	A.Xuat();
 	cout << endl << endl;
-	cout << "Nhap so can dem:  ";
+	
+	//dem so lan xuat hien 
+	cout << "Nhap so can dem: ";
 	int x;
 	cin >> x;
 	cout << x << " xuat hien trong mang " << A.demSoLanXuatHien(x) << " lan.\n\n";
 
+	//kiem tra mang tang dan
 	if (A.kiemTraTangDan())
 		cout << "Mang tang dan.\n";
 	else
 		cout << "Mang khong tang dan.\n";
 
+	//tim so le nho nhat va so nguyen to lon nhat
 	cout << endl;
 	cout << "So le nho nhat trong mang: " << A.timSoLeNhoNhat() << endl;
 	cout << "So nguyen to lon nhat trong mang: " << A.timSoNguyenToLonNhat() << endl << endl;
+
+	//sap xep tang dan va giam dan
 	cout << "Mang sau khi sap xep tang dan: \n";
 	A.sapXepTangDan();
 	A.Xuat();
